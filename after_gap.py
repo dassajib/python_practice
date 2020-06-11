9b4a58603ef4200e93dfd0bdfb61cb966fd3ed71
sajib <45949826+dassajib@users.noreply.github.com>
2020-06-12 00:26:31 +0600

    Add files via upload

---
+++ after_gap.py
@@ -0,0 +1,95 @@
+#print('After some days i am back')
+print('I have to more serious about coding')
+print('actually i had an issue so i cpuldnt')
+print('but technically i made a gap n its like ', 3+2,'days')
+print('we can write it like ',2*2+1, 'also')
+print('is it less then 1 week?', 7>5)
+
+print('how i reach Galachipa to Patuakhali i put it below')
+
+via = 'bus'
+seat_in_a_bus = 40
+can_seat = 20
+via2 = 'hunda'
+cost = 'money'
+via3 = 'ricksa'
+
+print('so normally i dont go kheyaghat by',via3)
+print('i pass the river by nouka and search for a',via)
+print('if bus schedule is late then i search for a',via2)
+print('bcz',via2,'is more expensive than',via)
+print('in bus generally there is',seat_in_a_bus,'seat')
+print('for corona sisuation now a days there ',can_seat,'people')
+print('people seat now after',seat_in_a_bus/can_seat,'by 2')
+
+
+
+my_work = 'work'
+my_work_1 = 'bring something that needed'
+my_work_2 = 'managed blood'
+my_work_3 = 'for mental support'
+
+
+print(f'lets talk about after reaching then about {my_work}')
+print(f'some time i help to {my_work_1}')
+print(f'some time i {my_work_2}')
+print(f'some time i was there for {my_work_3}')
+
+
+types_of_patient = 'two'
+print(f'there were {types_of_patient} types of patient')
+type1 = 'pregnant'
+type2 = 'ill kids'
+print(f'some of are {type1} patient and some of are {type2}')
+
+doctors = 'the doctors are very concern'
+sisters = 'sisters are very alert and.{}'
+
+print(sisters.format(doctors))
+
+A = 'i loved those kids '
+B = 'and i couldnt touch them for this bloody sisuation'
+
+print(A+B)
+
+
+print('i thought i will get a pichhi')
+print('with whom i can play')
+print('with whom i share love and{}.'.format(' i can spend best time'))
+print('.' * 100)
+A = 'i'
+B = 'd'
+C = 'i'
+D = 'd'
+E = 'n'
+F = 't'
+G = 's'
+H = 'e'
+I = 'e'
+J = 'h'
+K = 'i'
+L = 'm'
+
+print(A)
+print(B+C+D+E+F)
+print(G+H+I)
+print(J+K+L)
+
+
+formatter = '{} {} {} {} {}'
+print(formatter.format(1, 2, 3, 4, 5))
+print(formatter.format('W', 'O', 'R', 'S', 'E'))
+
+
+
+
+X = 'I saw a baby who died in her mothers belly'
+Y= 'his face i can remember now also unfortunately'
+
+print(X)
+print(Y)
+
+print(''' For sure if i saw my babies face
+then i couldnt forget him in my life
+he was only eight month
+God please bless my baby.''')
