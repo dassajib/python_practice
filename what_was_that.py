7592f37706740c488dcf8bd461aeb13bc23da1fe
sajib <45949826+dassajib@users.noreply.github.com>
2020-06-16 01:45:28 +0600

    Add files via upload

---
+++ what_was_that.py
@@ -0,0 +1,16 @@
+best_opener = 'Best OPener : Tamim Iqbal'
+best_all_rounder = 'Best All Rounder : Shakib Al Hasan'
+best_middle = 'Best Middle : Mahmudullah Riyad'
+best_baller = 'Best Baller : Mustafizur Rahman'
+
+best_five = """Best five Cricketer :
+\t *Shakib
+\t *Tamim
+\t *Musffiq
+\t *Mahmudullah \n\t *Mashrafee"""
+
+print(best_opener)
+print(best_all_rounder)
+print(best_middle)
+print(best_baller)
+print(best_five)
