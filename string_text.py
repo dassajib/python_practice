f98ba5efa21b52f6d7daeb894f9c8614b512e72d
sajib <45949826+dassajib@users.noreply.github.com>
2020-06-16 01:46:20 +0600

    Add files via upload

---
+++ string_text.py
@@ -0,0 +1,24 @@
+types_of_people = 2
+x = (f'there are {types_of_people}types of people')
+
+valo = 'love'
+valona = 'do not'
+
+y = (f'those are who {valo} football and who {valona} do not')
+
+print (x)
+print (y)
+
+print(f'i said: {x}')
+print(f'i also said: {y}')
+
+bad = 'they even donot love music'
+very_bad = 'some people do not watch football.{}'
+
+print(very_bad.format(bad))
+
+A = 'most of them are naturally bad.'
+B = 'ignore them'
+
+print(A+B)
+
