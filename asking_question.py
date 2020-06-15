9e02d1eac6ae68bf82dd5790bdf8833628a83443
sajib <45949826+dassajib@users.noreply.github.com>
2020-06-16 01:47:24 +0600

    Add files via upload

---
+++ asking_question.py
@@ -0,0 +1,23 @@
+print('My name is :',end =' ')
+name = input()
+print('I am from :', end = ' ')
+Elaka = input()
+print('i am studying in :', end =' ')
+Uni = input()
+
+print(f'So, i am {name},i am from {Elaka} and now i am studying in {Uni}')
+
+
+
+
+
+print('Now which sem are you in?', end = ' ')
+Semester = input()
+print('Which site you are going to in?', end = ' ')
+Interest = input()
+print('with which language you are working?', end = ' ')
+Language = input()
+print('In future with which framework you will working?', end = ' ')
+Framework = input()
+
+print(f'Now i am in {Semester} semester\n i am going to in {Interest}\n I am working with {Language}\n I will working with {Framework} hopefully')
